d3b46c7c9430a779a6c1ec23540157bedeac6802
hmagalhaesrio <hmagalhaesrio@hawk.iit.edu>
2025-10-30 08:31:22 -0500

    Files

--- SB3.py
+++ SB3.py
@@ -2,14 +2,14 @@ import gymnasium as gym
 
 from stable_baselines3 import A2C
 
-env = gym.make("InvertedPendulum-v5", render_mode="rgb_array")
+env = gym.make("Ant-v5", render_mode="rgb_array")
 
 model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=100_000)
+model.learn(total_timesteps=1000_000)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(5000):
+for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render("human")
